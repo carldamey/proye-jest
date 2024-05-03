6c9f71582cadaee50626541528e39e166695f71a
carldamey <carldamey@gmail.com>
2024-05-03 16:12:41 -0700

    further expands jest test cases, refactors file names to avoid conflicts

--- proye-jest.test.js
+++ proye-jest.test.js
@@ -1,10 +1,4 @@
-function isEven(num) {
-  return num % 2 === 0
-}
-
-console.log(isEven(2))
-console.log(isEven(1))
-
 test("It should return true when called with an even number", () => {
   expect(isEven(2)).toBe(true)
+  expect(isEven(1)).toBe(false)
 })
